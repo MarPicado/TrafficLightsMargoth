7ec0b1e3cbaa558d29da26c60fee6591585bd5c7
Margoth <mloreana@gmail.com>
2021-03-09 23:30:04 +0000

    traffic light

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,23 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import { Red } from "./red";
-import { Yellow } from "./yellow";
-import { Green } from "./green";
 
 //create your first component
+
 export function Home() {
+	const [color, setColor] = useState("red");
+	const allColors = ["red", "yellow", "green"];
 	return (
 		<div className="trafficlight">
-			<div className="red">
-				<Red />
-			</div>
-			<div className="yellow">
-				<Yellow />
-			</div>
-			<div className="green">
-				<Green />
-			</div>
+			{allColors.map(c => (
+				<div
+					className={`light ${c} ${color === c ? "glow" : ""}`}
+					onClick={() => setColor(c)}>
+					{c}
+				</div>
+			))}
 		</div>
 	);
 }
